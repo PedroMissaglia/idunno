584bc9d1a62d2718c26f963d26d0191c8baa5f68
PedroMissaglia <pedro.missaglia@gmail.com>
2021-08-11 17:18:09 -0300

    about

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,17 +1,11 @@
-import { AboutPage } from './../about/about.page';
-import { MessageComponent } from './../message/message.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { AboutPage } from './about.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
-  },
-  {
-    path: 'about',
     component: AboutPage
   }
 ];
@@ -20,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class AboutPageRoutingModule {}
