894341b111981372095765fb970f69be82a08abf
PedroMissaglia <pedro.missaglia@gmail.com>
2021-08-12 08:59:24 -0300

    idunno

--- src/app/chat/back-end/index.js
+++ src/app/chat/back-end/index.js
@@ -1,23 +1,30 @@
 let app = require('express')();
 let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+let io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    credentials: true
+  }
+});
 
-io.on('connection', socket => {
-    socket.on('disconnect', () => {
-        io.emit('users-changed', {user: socket.username, event: 'left'});
-    });
-    socket.on('set-name', name => {
-        socket.username = name;
-        io.emit('users-changed', {user: socket.username, event: 'left'});
-    });
-    socket.on('send-message', message => {
-        io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date() })
-    });
+io.on('connection', (socket) => {
 
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});
+  });
+
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});
+  });
+
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});
+  });
 });
 
 var port = process.env.PORT || 3001;
 
-server.listen(port, () => {
-    console.log('server in localhost:' + port);
-})
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
