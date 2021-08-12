7bf4b76830464300e4c28b4154a31ec9c2c5b1a2
Pedro Missaglia <pedro.missaglia@gmail.com>
2021-08-12 18:08:17 -0300

    idunno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBNCnXpQSaN4LOu2KG02IJK-FALgA6MP-8",
+    authDomain: "idunno-1d89f.firebaseapp.com",
+    projectId: "idunno-1d89f",
+    storageBucket: "idunno-1d89f.appspot.com",
+    messagingSenderId: "1074839435501",
+    appId: "1:1074839435501:web:1b1096f2f6abde5f5bf4e4"
+  }
 };
